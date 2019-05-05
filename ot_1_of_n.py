54f4246f09b1d733560a33fb69e95111a2a150ed
Piotr Szyma <thompson2908@gmail.com>
2019-05-05 13:46:51 +0200

    1 of n fixed, PSI introduced

--- ot_1_of_n.py
+++ ot_1_of_n.py
@@ -1,4 +1,5 @@
 """Implementation of Oblivious Transfer 1 of n protocol."""
+import random
 from charm.core.math.integer import integer  # pylint: disable=no-name-in-module
 from charm.toolbox.integergroup import IntegerGroupQ
 
@@ -10,28 +11,27 @@ g = G.randomGen()
 
 def main():
   # A got messages.
-  msgs = [G.random() for _ in range(NUMBER_OF_MESSAGES)]
+  msgs = [g ** G.random() for _ in range(NUMBER_OF_MESSAGES)]
   rs = [G.random() for _ in msgs]
   Rs = [g ** r for r in rs]
   # A sends Rs to B.
 
   # B chooses random Alpha & index k
-  # k = random.choice(range(len(msgs)))
-  k = 0
+  k = random.choice(range(len(msgs)))
+  # k = 0
   alpha = G.random()
   X = Rs[k] ** alpha
   # B sends X to A.
 
   # A calculates Ws.
   ws = [X ** (1/rs_i) for rs_i in rs]
-  zs = [(m_i * w_i) % G.q for m_i, w_i in zip(msgs, ws)]
+  zs = [m_i * w_i for m_i, w_i in zip(msgs, ws)]
   # A sends cs to B.
 
   # B checks message.
-  msg = zs[k] / integer(g ** alpha, G.q)
+  msg = zs[k] / g ** alpha
 
   # Verification.
-  import pdb; pdb.set_trace()
   assert msg == msgs[k]
 
 if __name__ == "__main__":
---
+++ privacy_set_intersection.py
@@ -0,0 +1,46 @@
+"""Implementation of Privacy Set Intersection protocol.
+
+It is used when two parties want to get common elements without knowledge
+about all elements they possess.
+"""
+from charm.toolbox.integergroup import IntegerGroupQ
+
+NUMBER_OF_MESSAGES = 5
+SECURITY_PARAM = 20
+G = IntegerGroupQ()
+G.paramgen(SECURITY_PARAM)
+g = G.randomGen()
+
+
+def main():
+  # Elements generation phase, AnB contains similar elements.
+  common = [g ** G.random() for _ in range(3)]
+
+  # A generates hashes H_a and sends them to B.
+  A = [g ** G.random() for _ in range(7)] + common
+  a_k = G.random()
+  H_a = [G.hash(a) ** a_k for a in A]
+
+
+  # B generates hashes H_b and sends them to A.
+  B = [g ** G.random() for _ in range(7)] + common
+  b_k = G.random()
+  H_b = [G.hash(b) ** b_k for b in B]
+
+
+  # B powers hashes of A to his secret b.
+  H_a_b = [a ** b_k for a in H_a]
+
+  # A powers hashes of B to his secret a.
+  H_b_a = [b ** a_k for b in H_b]
+
+  # A computes common.
+  a_common = [o for o, e in zip(A, H_a_b) if e in H_b_a]
+
+  # B computes common.
+  b_common = [o for o, e in zip(A, H_a_b) if e in H_b_a]
+
+  assert a_common == b_common == common
+
+if __name__ == "__main__":
+    main()
